e217303f7627bdf7c2b7a435f7ea62a79308ff1e
Sindre Vaskinn Hunn <sindre.hunn@siemens.com>
2025-10-26 22:29:54 +0100

    updated normalize (save and load pkl)

--- src/convert_file_for_ML.py
+++ src/convert_file_for_ML.py
@@ -1,5 +1,6 @@
 import json
-from pandas import json_normalize
+import pandas as pd
+import os
 
 import setup
 
@@ -8,7 +9,7 @@ def noramlise_json_data():
         with open(setup.output_time_series_file, 'r') as f:
             data = json.load(f)
             
-            df = json_normalize(data)
+            df = pd.json_normalize(data)
             print(f"output_time_series_file normalised and returned as dataframe")
             
             return df
@@ -16,3 +17,12 @@ def noramlise_json_data():
     except Exception as e:
         print(f"Failed to load output_time_series_file, {e}")
         return None
+    
+def save_normalized_df(df, file_path):
+    df.to_pickle(file_path)
+
+def load_normalized_df(file_path):
+    if os.path.isfile(file_path):
+        return pd.read_pickle(file_path)
+    else:
+        print(f"ERROR: Failed to load normalised data frame from {file_path}")
